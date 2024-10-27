a3276898259c078a05cb93b4cfa13f782f550add
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-27 21:55:55 +0900

    解答

--- ABC_class/abc202/b.rb
+++ ABC_class/abc202/b.rb
@@ -6,6 +6,22 @@
 # S を反転する。
 # 0を0に1を1に6を9に8を8に9を6に変換する。
 
-string = gets
+string = gets.chomp.split("")
+new_string = []
 
-puts "数列#{string}"
+# 180°反転
+string.length.times do |i|
+  if string[string.length-i-1] == "6"
+    new_string << "9"
+  elsif string[string.length-i-1] == "9"
+    new_string << "6"
+  else
+    new_string << string[string.length-i-1]
+  end
+end
+
+# 出力
+new_string.each do |a|
+  print a
+end
+print "\n"
