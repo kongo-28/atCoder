e33e0c3467a2c214839c6bb513bb7ba84438473a
kongo-28 <ryota.tennis.ku@gmail.com>
2024-11-04 23:33:18 +0900

    Update c.rb

--- ABC_class/abc204/c.rb
+++ ABC_class/abc204/c.rb
@@ -15,12 +15,53 @@
 # スタート地点とゴール地点の都市の組として
 # 考えられるものは何通りありますか？
 
+########### 再帰関数を定義 ################
+def serch_roads(i,j)
+  @roads.each do |road|
+    if road[0] == i
+      if road[1] == j
+        @count += 1
+        puts "再帰的countした!!!"
+        return
+      else
+        serch_roads(road[1],j)
+      end
+    end
+  end
+end
+########### 再帰関数を定義 ################
+
 city_number, road_number = gets.split.map(&:to_i)
-roads = []
+@roads = []
 
 road_number.times do 
   road = gets.split.map(&:to_i)
-  roads << road
+  @roads << road
 end
 
-p roads
+p @roads
+
+i = 1
+j = 1
+@count = 0 
+
+while i <= city_number
+  j = 1
+
+  while j <= city_number
+
+    puts "(#{i}, #{j})"
+
+    if i == j
+      @count += 1
+      puts "countした"
+    else    ##### i != j の場合 #####
+      serch_roads(i,j)
+    end
+
+    j += 1
+  end
+  i += 1
+end
+
+puts "count = #{@count}"
